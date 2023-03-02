4ac7d268cb075f909699476e25947b131fa6c6e8
Ibrahim Taib <taibi@ensipc289.ensimag.fr>
2023-03-02 19:17:36 +0100

    minmax algorithm

--- blobwar/src/strategy/minmax.rs
+++ blobwar/src/strategy/minmax.rs
@@ -4,15 +4,61 @@ use crate::configuration::{Configuration, Movement};
 use crate::shmem::AtomicMove;
 use std::fmt;
 
-/// Min-Max algorithm with a given recursion depth.
+/// Min-Max algorithm with a given recursion depth.ch
 pub struct MinMax(pub u8);
 
 impl Strategy for MinMax {
     fn compute_next_move(&mut self, state: &Configuration) -> Option<Movement> {
-        unimplemented!("TODO: implementer min max")
+        //unimplemented!("TODO: implementer min max");
+        let (movement, _) = MinMax::min_max(self, state, self.0, state.current_player);
+        movement
     }
 }
+impl MinMax{
+    fn min_max(&mut self, state: &Configuration, depth: u8, is_maximizing_player: bool) -> (Option<Movement>, i8) {
+        if depth == 0 {
+            return (None, if state.current_player {state.value()} else {-state.value()});
+        }
+        //println!("{depth}");
+        let mut value;
+        let mut best_movement: Option<Movement> = None;
 
+        if !is_maximizing_player {
+            //maximizing player
+            value = i8::MIN;
+
+            for movement in state.movements(){
+
+                // We play the current move
+                // let new_state = state.clone();
+                // new_state.play(&movement);
+                let (_ , new_state_val) = self.min_max(&state.play(&movement), depth - 1, !is_maximizing_player);
+                if value < new_state_val {
+                    value = new_state_val;
+                    best_movement = Some(movement);
+                }
+            }
+            
+        } else {
+            //minimizing player
+            value = i8::MAX;
+
+            for movement in state.movements(){
+
+                // let new_state = state.clone();
+                // new_state.play(& movement);
+
+                let (_ , new_state_val) = self.min_max(&state.play(&movement), depth - 1, !is_maximizing_player);
+
+                if value > new_state_val {
+                    value = new_state_val;
+                    best_movement = Some(movement);
+                }
+            }  
+        }
+        return (best_movement, value) ;
+    }
+}
 impl fmt::Display for MinMax {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "Min - Max (max level: {})", self.0)
