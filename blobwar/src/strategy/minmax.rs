e3f04578e1b81dadc6e29cfefdc5f118e214ce0e
Ibrahim Taib <ibrahim.taib@grenoble-inp.org>
2023-03-04 19:02:40 +0100

    Implementing parallel algorithm for min_max

--- blobwar/src/strategy/minmax.rs
+++ blobwar/src/strategy/minmax.rs
@@ -2,6 +2,7 @@
 use super::Strategy;
 use crate::configuration::{ Configuration, Movement };
 use crate::shmem::AtomicMove;
+use rayon::prelude::*;
 use std::fmt;
 
 /// Min-Max algorithm with a given recursion depth.ch
@@ -11,11 +12,12 @@ impl Strategy for MinMax {
     fn compute_next_move(&mut self, state: &Configuration) -> Option<Movement> {
         //let (movement, _) = MinMax::min_max(self, state, self.0, state.current_player);
 
-        let (movement, _) = MinMax::min_max_two(self, state, self.0, false);
+        let (movement, _) = MinMax::min_max_par(self, state, self.0, false);
         movement
     }
 }
 impl MinMax {
+    /// Classic min-max algorithm
     fn min_max(
         &mut self,
         state: &Configuration,
@@ -69,6 +71,8 @@ impl MinMax {
         }
         return (best_movement, value);
     }
+
+    /// Shorter, smarter min-max algorithm (only minimize)
     fn min_max_two(&mut self, state: &Configuration, depth: u8, opposing_player:bool) -> (Option<Movement>, i8){
         if depth == 0 {
             // state.value() indicates the loss of the current player
@@ -77,7 +81,7 @@ impl MinMax {
 
         let mut value;
         let mut best_movement: Option<Movement> = None;
-
+        
         // Minimize loss for our player
         value = i8::MAX;
 
@@ -102,6 +106,45 @@ impl MinMax {
             value = - value;
         }
         (best_movement, value)
+    }
+    
+    /// Parallel min-max algorithm
+    fn min_max_par(&mut self, state: &Configuration, depth: u8, opposing_player:bool) -> (Option<Movement>, i8){
+        if depth == 0 {
+            // state.value() indicates the loss of the current player
+            return (None, state.value());
+        }
+
+        let value = i8::MIN;
+        let best_movement: Option<Movement> = None;
+
+        // Minimize loss for our player
+
+        let values= state.movements().map(|movement| {
+            // We play the current move
+
+            let (_, new_state_val) = self.min_max_par(
+                &state.play(&movement),
+                depth - 1,
+                !opposing_player
+            );
+            // If opposing player, we want to maximize the loss
+            // => minimizing the gain
+            //if opposing_player {new_state_val = -new_state_val;}
+            (Some(movement), new_state_val)
+        });
+
+        let result;
+        if opposing_player {
+            result = values.max_by_key(|(_, value)| *value);
+        }else{
+            result = values.min_by_key(|(_, value)| *value);
+        }
+        
+        match result {
+            Some(some) => some,
+            None => {if opposing_player {(best_movement, -value)} else {(best_movement, value)}},
+        }
     }
 }
 impl fmt::Display for MinMax {
