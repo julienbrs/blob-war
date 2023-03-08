e3366b4c344c6df0a85d6d0cabe387bf465e8df1
Ibrahim Taib <ibrahim.taib@grenoble-inp.org>
2023-03-08 09:26:53 +0100

    gitignore not working D:

--- blobwar/target/debug/deps/semver-b67022c086ded05d.d
+++
@@ -1,9 +0,0 @@
-/user/2/.base/bourseaj/home/ensimag/2a/blob-war/blobwar/target/debug/deps/semver-b67022c086ded05d.rmeta: /user/2/bourseaj/.cargo/registry/src/github.com-1ecc6299db9ec823/semver-0.1.20/src/lib.rs /user/2/bourseaj/.cargo/registry/src/github.com-1ecc6299db9ec823/semver-0.1.20/src/version.rs /user/2/bourseaj/.cargo/registry/src/github.com-1ecc6299db9ec823/semver-0.1.20/src/version_req.rs
-
-/user/2/.base/bourseaj/home/ensimag/2a/blob-war/blobwar/target/debug/deps/libsemver-b67022c086ded05d.rlib: /user/2/bourseaj/.cargo/registry/src/github.com-1ecc6299db9ec823/semver-0.1.20/src/lib.rs /user/2/bourseaj/.cargo/registry/src/github.com-1ecc6299db9ec823/semver-0.1.20/src/version.rs /user/2/bourseaj/.cargo/registry/src/github.com-1ecc6299db9ec823/semver-0.1.20/src/version_req.rs
-
-/user/2/.base/bourseaj/home/ensimag/2a/blob-war/blobwar/target/debug/deps/semver-b67022c086ded05d.d: /user/2/bourseaj/.cargo/registry/src/github.com-1ecc6299db9ec823/semver-0.1.20/src/lib.rs /user/2/bourseaj/.cargo/registry/src/github.com-1ecc6299db9ec823/semver-0.1.20/src/version.rs /user/2/bourseaj/.cargo/registry/src/github.com-1ecc6299db9ec823/semver-0.1.20/src/version_req.rs
-
-/user/2/bourseaj/.cargo/registry/src/github.com-1ecc6299db9ec823/semver-0.1.20/src/lib.rs:
-/user/2/bourseaj/.cargo/registry/src/github.com-1ecc6299db9ec823/semver-0.1.20/src/version.rs:
-/user/2/bourseaj/.cargo/registry/src/github.com-1ecc6299db9ec823/semver-0.1.20/src/version_req.rs:
