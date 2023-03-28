9f40dfe140e4c1f245daa49e1b70b9c96fcc2ab5
Ibrahim Taib <ibrahim.taib@grenoble-inp.org>
2023-03-28 12:26:07 +0200

    alphabeta

--- blobwar/src/strategy/alphabeta.rs
+++ blobwar/src/strategy/alphabeta.rs
@@ -28,6 +28,74 @@ impl fmt::Display for AlphaBeta {
 
 impl Strategy for AlphaBeta {
     fn compute_next_move(&mut self, state: &Configuration) -> Option<Movement> {
-        unimplemented!("implementer alpha beta")
+        let (mut alpha, mut beta) = (i8::MIN, i8::MAX);
+        let m = self.elagage_no_par(state, self.0, &alpha, &beta, false).0;
+        match m {
+            Some(m) => {
+                println!("{:?}", m);
+                None
+            }
+            None => None,
+        }
+    }
+}
+
+impl AlphaBeta {
+    fn elagage_no_par(
+        &mut self,
+        state: &Configuration,
+        depth: u8,
+        alpha: &i8,
+        beta: &i8,
+        minimize_player: bool,
+    ) -> (Option<Movement>, i8) {
+        println!("alpha {:} beta {:}", alpha, beta);
+        if depth == 0 {
+            return (None, state.value());
+        }
+        let mut a = *alpha;
+        let mut b = *beta;
+        if minimize_player {
+            let mut v = i8::MAX;
+            for movement in state.movements() {
+                v = std::cmp::min(
+                    v,
+                    self.elagage_no_par(
+                        &state.play(&movement),
+                        depth - 1,
+                        &a,
+                        &b,
+                        !minimize_player,
+                    )
+                    .1,
+                );
+                if v >= b {
+                    return (Some(movement), v);
+                }
+                b = std::cmp::min(b, v);
+            }
+        } else {
+            let mut v = i8::MIN;
+            for movement in state.movements() {
+                println!("here");
+                v = std::cmp::max(
+                    v,
+                    self.elagage_no_par(
+                        &state.play(&movement),
+                        depth - 1,
+                        &a,
+                        &b,
+                        !minimize_player,
+                    )
+                    .1,
+                );
+                if a >= v {
+                    return (Some(movement), v);
+                }
+                println!("alpha treated {:}", a);
+                a = std::cmp::max(a, v);
+            }
+        }
+        return (None, state.value());
     }
 }
